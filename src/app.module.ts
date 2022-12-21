c07f7c295dc40476bf4c8458e5fd64a7a6415e2e
Artur <82472015+benyaar@users.noreply.github.com>
2022-12-21 23:16:50 +0200

    13HW change db url

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,29 +6,30 @@ import { UsersModule } from './users/user.module';
 import { BlogsModule } from './blogs/blogs.module';
 import { PostsModule } from './post/posts.module';
 
-// @Module({
-//   imports: [
-//     UsersModule,
-//     BlogsModule,
-//     MongooseModule.forRoot(
-//       'mongodb+srv://admin:admin@backapi.wojaaxk.mongodb.net/?retryWrites=true&w=majority',
-//     ),
-//   ],
-//   controllers: [AppController],
-//   providers: [AppService],
-// })
-// export class AppModule {}
-
 @Module({
   imports: [
     UsersModule,
     BlogsModule,
     PostsModule,
     MongooseModule.forRoot(
-      'mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.6.1',
+      'mongodb+srv://admin:admin@backapi.wojaaxk.mongodb.net/?retryWrites=true&w=majority',
     ),
   ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+
+// @Module({
+//   imports: [
+//     UsersModule,
+//     BlogsModule,
+//     PostsModule,
+//     MongooseModule.forRoot(
+//       'mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.6.1',
+//     ),
+//   ],
+//   controllers: [AppController],
+//   providers: [AppService],
+// })
+// export class AppModule {}
