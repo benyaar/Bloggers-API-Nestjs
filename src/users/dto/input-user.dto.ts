62565ea37635c50cbf166132236bbc1663738d7b
Artur <82472015+benyaar@users.noreply.github.com>
2022-12-24 10:34:31 +0200

    13HW delete validation input login

--- src/users/dto/input-user.dto.ts
+++ src/users/dto/input-user.dto.ts
@@ -1,7 +1,6 @@
-import { IsEmail, Length, Matches } from 'class-validator';
+import { IsEmail, Length } from 'class-validator';
 
 export class InputUserDto {
-  @Matches('^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$\n')
   @Length(3, 10)
   login: string;
   @Length(6, 20)
