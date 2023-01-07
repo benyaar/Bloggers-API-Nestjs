fe8f1f48993a7109b3aea84d0680833274da6e7a
Artur <82472015+benyaar@users.noreply.github.com>
2023-01-07 22:50:02 +0200

    14HW add basic auth on create user

--- src/auth/controller/auth.controller.ts
+++ src/auth/controller/auth.controller.ts
@@ -1,4 +1,11 @@
-import { Controller, Post, Body, UseGuards, Res } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  UseGuards,
+  Res,
+  HttpCode,
+} from '@nestjs/common';
 import { AuthService } from '../application/auth.service';
 import { RegistrationDto } from '../dto/registration.dto';
 import { LocalAuthGuard } from '../guards/local-auth.guard';
@@ -11,12 +18,14 @@ export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('registration')
+  @HttpCode(204)
   async registration(@Body() registrationDto: RegistrationDto) {
     return this.authService.registration(registrationDto);
   }
 
   @UseGuards(LocalAuthGuard)
   @Post('login')
+  @HttpCode(200)
   async login(@User() user, @Res({ passthrough: true }) response: Response) {
     const JwtPair = await this.authService.login(user);
     response.cookie('refreshToken', JwtPair.refreshToken, {
--- src/users/controller/users.controller.ts
+++ src/users/controller/users.controller.ts
@@ -8,6 +8,7 @@ import {
   Param,
   Post,
   Query,
+  UseGuards,
 } from '@nestjs/common';
 
 import { UsersService } from '../application/users.service';
@@ -17,6 +18,7 @@ import {
   PaginationInputDTO,
   PaginationUserInputDTO,
 } from '../../helpers/dto/helpers.dto';
+import { BasicAuthGuard } from '../../auth/guards/basic-auth.guard';
 
 @Controller('users')
 export class UsersController {
@@ -25,21 +27,26 @@ export class UsersController {
     public queryUserRepository: UsersQueryRepository,
   ) {}
 
+  @UseGuards(BasicAuthGuard)
   @Post()
   async createUser(@Body() inputUserDto: InputUserDto) {
     return this.usersService.createUser(inputUserDto);
   }
+
   @Get()
   //getUsers(@Query('term') term: string ) - или так getUsers(@Query() query: Type )
   async findAllUsers(@Query() paginationInputDTO: PaginationUserInputDTO) {
     return this.queryUserRepository.findAllUsers(paginationInputDTO);
   }
+
   @Get(':id')
   async findUserById(@Param('id') id: string) {
     const findUserById = await this.queryUserRepository.findUserById(id);
     if (!findUserById) throw new NotFoundException([]);
     return findUserById;
   }
+
+  @UseGuards(BasicAuthGuard)
   @Delete(':id')
   @HttpCode(204)
   async deleteUserById(@Param('id') id: string) {
