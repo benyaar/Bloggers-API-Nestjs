89730d8a5d571534dbe4769060e41aa831c0f7c6
Artur <82472015+benyaar@users.noreply.github.com>
2023-01-14 23:42:09 +0200

    14HW add model for clear db

--- src/testing/delete-all.module.ts
+++ src/testing/delete-all.module.ts
@@ -7,6 +7,12 @@ import { DeleteAllController } from './delete-all.controller';
 import { DeleteAllRepository } from './delete-all.repository';
 import { User, UserSchema } from '../users/schemas/user.schema';
 import { Comment, CommentsSchema } from '../comments/schema/comments.schema';
+import { Attempt, AttemptSchema } from '../auth/schemas/attempts.schema';
+import {
+  TokenBlackList,
+  TokenBlackListSchema,
+} from '../auth/schemas/token-blacklist.schema';
+import { Device, DeviceSchema } from '../devices/schemas/devices.schema';
 
 @Module({
   imports: [
@@ -15,6 +21,9 @@ import { Comment, CommentsSchema } from '../comments/schema/comments.schema';
       { name: Blog.name, schema: BlogSchema },
       { name: User.name, schema: UserSchema },
       { name: Comment.name, schema: CommentsSchema },
+      { name: Attempt.name, schema: AttemptSchema },
+      { name: TokenBlackList.name, schema: TokenBlackListSchema },
+      { name: Device.name, schema: DeviceSchema },
     ]),
   ],
   controllers: [DeleteAllController],
--- src/testing/delete-all.repository.ts
+++ src/testing/delete-all.repository.ts
@@ -6,6 +6,12 @@ import { Blog, BlogsDocument } from '../blogs/schemas/blogs.schema';
 import { Post, PostsDocument } from '../post/schemas/post.schema';
 import { User, UsersDocument } from '../users/schemas/user.schema';
 import { Comment, CommentsDocument } from '../comments/schema/comments.schema';
+import { Attempt, AttemptsDocument } from '../auth/schemas/attempts.schema';
+import {
+  TokenBlackList,
+  TokenBlackListDocument,
+} from '../auth/schemas/token-blacklist.schema';
+import { Device, DeviceDocument } from '../devices/schemas/devices.schema';
 
 @Injectable()
 export class DeleteAllRepository {
@@ -18,12 +24,21 @@ export class DeleteAllRepository {
     private readonly usersModel: Model<UsersDocument>,
     @InjectModel(Comment.name)
     private readonly commentsModel: Model<CommentsDocument>,
+    @InjectModel(Attempt.name)
+    private readonly attemptModel: Model<AttemptsDocument>,
+    @InjectModel(TokenBlackList.name)
+    private readonly tokenBlackListModel: Model<TokenBlackListDocument>,
+    @InjectModel(Device.name)
+    private readonly deviceModel: Model<DeviceDocument>,
   ) {}
   async deleteAll() {
     await this.blogsModel.deleteMany();
     await this.postsModel.deleteMany();
     await this.usersModel.deleteMany();
     await this.commentsModel.deleteMany();
+    await this.attemptModel.deleteMany();
+    await this.tokenBlackListModel.deleteMany();
+    await this.deviceModel.deleteMany();
     return;
   }
 }
