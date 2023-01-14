96e8255d4f69988dc02d8b64a852c5aa0af47d37
Artur <82472015+benyaar@users.noreply.github.com>
2023-01-15 00:42:02 +0200

    14HW change refresh token time

--- src/auth/application/auth.service.ts
+++ src/auth/application/auth.service.ts
@@ -56,11 +56,11 @@ export class AuthService {
 
     const jwtPair = {
       accessToken: this.jwtService.sign(payload, {
-        expiresIn: '1000s',
+        expiresIn: '10s',
         secret: JWT.jwt_secret,
       }),
       refreshToken: this.jwtService.sign(payload, {
-        expiresIn: '2000s',
+        expiresIn: '20s',
         secret: JWT.jwt_secret,
       }),
     };
